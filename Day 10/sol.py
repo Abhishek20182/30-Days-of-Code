ced4de89ad9d2355968f917b96e3b48ef370b170
Abhishek Tiwari <45140530+Abhishek20182@users.noreply.github.com>
2021-07-11 11:44:01 +0530

    Day 10

---
+++ Day 10/sol.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    count = 0
+    while n:
+        n &= n<<1
+        count += 1
+    
+    print(count)
